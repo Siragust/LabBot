0b614526c2351c2c5ed353a2dab96ac0c553cdb1
Siragust <98845220+Siragust@users.noreply.github.com>
2023-12-21 10:31:59 +0200

    LabBot.py

--- LabBot.py
+++ LabBot.py
@@ -10,13 +10,13 @@ bot.remove_command('help')
 #greeting
 @bot.event
 async def on_ready():
-    print("Бот работает")
+    print("бот працює")
 
 #greeting2
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send('Добро пожаловать на сервер!')
+    await member.dm_channel.send('Ласкаво просимо на сервер!')
 
 #help
 @bot.command()
@@ -25,7 +25,7 @@ async def help(ctx):
     embed = discord.Embed(
         color = 0x7CFC00,
         title = 'Список команд:',
-        description = '$help - Вызов этого списка\n$kick "@user" - кик пользователя с сервера\n$rand - вывод случайного числа радиусом 0 - 500'
+        description = '$help - Виклик цього списку\n$kick "@user" - кік користувача із сервера\n$rand - виведення випадкового числа радіусом 0 - 500'
         )
     await ctx.send(embed = embed)
 
@@ -39,7 +39,7 @@ async def rand(ctx):
 async def kick(ctx, member : discord.Member):
     await ctx.message.delete()
     await member.kick()
-    await ctx.send("Пользователь {} кикнут с сервера".format(member.mention))
+    await ctx.send("Користувач {} кікнут із сервера".format(member.mention))
 
 
 bot.run("MTE4NDk2NjQ1OTY2ODA0MTg2OQ.G2UzbN.FSkDdNTa40XSmvMG1YXqK2-TAQiXiuMoo-zK4o")
